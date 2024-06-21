2e23a472d21f0ef2a11943564ab3a5f4845af091
debuserkvm2 <debuserkvm2@debhostnamekvm2.debkvm2.org>
2024-06-21 18:32:58 +0800

    initial working on debian

---
+++ config/puma.rb
@@ -0,0 +1,17 @@
+# config/puma.rb
+
+# Change to match your CPU core count
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+
+# Min and Max threads per worker
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads threads_count, threads_count
+
+# Specifies the `port` that Puma will listen on to receive requests, default is 3000
+port ENV.fetch('PORT') { 3000 }
+
+# Specifies the `environment` that Puma will run in
+environment ENV.fetch('RAILS_ENV') { 'development' }
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
